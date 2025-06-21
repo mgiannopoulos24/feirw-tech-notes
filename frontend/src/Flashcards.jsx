857e5ca4682492f296729ffa4dc95ed170e361e2
a-reynbaw <ammarinakou@gmail.com>
2025-06-21 23:10:59 +0300

    flashcards changed

--- frontend/src/Flashcards.jsx
+++ frontend/src/Flashcards.jsx
@@ -5,25 +5,25 @@ const Flashcards = () => {
   const cards = [
     {
       id: 1,
-      frontHTML: <div>Τι είναι η μεταβλητή;</div>,
-      backHTML: <>Θέση μνήμης στην οποία μπορούμε να αποθηκεύσουμε δεδομένα.</>,
+      frontHTML: <div className="flex h-full items-center justify-center text-center text-2xl">Τι είναι η μεταβλητή;</div>,
+      backHTML: <div className="flex h-full items-center justify-center text-center text-2xl">Θέση μνήμης στην οποία μπορούμε να αποθηκεύσουμε δεδομένα.</div>,
     },
     {
       id: 2,
-      frontHTML: <>Ποια εντολή χρησιμοποιούμε για είσοδο δεδομένων στην ΓΛΩΣΣΑ;</>,
-      backHTML: <>ΔΙΑΒΑΣΕ</>,
+      frontHTML: <div className="flex h-full items-center justify-center text-center text-2xl">Ποια εντολή χρησιμοποιούμε για είσοδο δεδομένων στην ΓΛΩΣΣΑ;</div>,
+      backHTML: <div className="flex h-full items-center justify-center text-center text-2xl">ΔΙΑΒΑΣΕ</div>,
     },
     {
       id: 3,
-      frontHTML: <>Ποια εντολή χρησιμοποιούμε για έξοδο δεδομένων στην ΓΛΩΣΣΑ;</>,
-      backHTML: <>ΓΡΑΨΕ</>,
+      frontHTML: <div className="flex h-full items-center justify-center text-center text-2xl">Ποια εντολή χρησιμοποιούμε για έξοδο δεδομένων στην ΓΛΩΣΣΑ;</div>,
+      backHTML: <div className="flex h-full items-center justify-center text-center text-2xl">ΓΡΑΨΕ</div>,
     },
   ];
 
   return (
     <div className="flex justify-center items-center">
       <div className="w-full max-w-xl">
-        <FlashcardArray cards={cards} />
+        <FlashcardArray cards={cards} style={{ width: 400, height: 300 }}/>
       </div>
     </div>
   );
